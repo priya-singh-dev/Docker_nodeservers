6ca433400080c625666b9bdadd395c87fbc67f5f
Priya Singh <priyasingh2996@gmail.com>
2018-10-31 23:05:58 +0530

    file updated again

--- server2.js
+++ server2.js
@@ -9,7 +9,7 @@ var app = http.createServer(function (req, res) {
 
     res.write(JSON.stringify({ type:'Sport news', date : '26 March, 18', news :'BUENOS AIRES: Argentina captain Lionel Messi admitted Monday that his generation has a score to settle at the World Cup in Russia after a series of agonising near misses in major tournaments.' } ,null,3) ); //news 1
 
-    res.write("\nSport News 2\n");z
+    res.write("\nSport News 2\n");
 
     res.write(JSON.stringify({ type:'Sport news', date : '27 March, 18', news :' Olympic and world championship silver medallist PV Sindhu on Tuesday suffered an injury scare but there is no immediate threat to her participation in the Commonwealth Games starting next month. ' } ,null,3) ); //news 2
 
